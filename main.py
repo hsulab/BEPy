b6058941b68b68d2e0f0f64c5e1ebc338cedc517
hsulab <ahcigar@foxmail.com>
2017-12-20 17:41:13 +0800

    Upadates for MsXsdfile.

--- main.py
+++ main.py
@@ -8,15 +8,25 @@ Created on Thu Dec  7 16:23:00 2017
 # import libs
 import sys
 import os
+import pandas as pd
 ### my own lib
 import find_lib as fd      
-import calculate_lib as cal
+import geometry
+import dirtools_lib as dirtool
 # main func
 def main():
-    try:
-
-    except Exception as e:
-        print(r'Something wrong!')  
+    #try:
+        angle_assemble_CMO_ts = dirtool.walk_dir_cal_bond_angle_dic(r'./database/CMO_ts')
+        df_CMO_ts=pd.DataFrame(angle_assemble_CMO_ts).T
+        angle_assemble_CMO_fs = dirtool.walk_dir_cal_bond_angle_dic(r'./database/CMO_fs')
+        df_CMO_fs=pd.DataFrame(angle_assemble_CMO_fs).T
+        with pd.ExcelWriter(r'./data_in_excel/CMO_ts.xls') as writer:
+            df_CMO_ts.to_excel(writer, '0')
+        with pd.ExcelWriter(r'./data_in_excel/CMO_fs.xls') as writer:
+            df_CMO_fs.to_excel(writer, '0')
+        #print(cal.calculate_bond_angle_fiel1file(r'./database/example1.xsd'))
+    #except Exception as e:
+        #print(r'Something wrong!')  
 
 ###
 if __name__ == '__main__':
