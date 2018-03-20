228cb8019a775becec509371dc937c8f0f05f30d
许嘉琰 <ahcigar@foxmail.com>
2018-03-20 14:24:51 +0800

    autoSet and autoVASP

---
+++ autoVASP/auto4CAR.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#########################################################################
+# File Name: auto4CAR.py
+# Author: jyxu
+# mail: ahcigar@foxmail.com
+# Created Time: 一  3/19 21:15:34 2018
+#########################################################################
+import os
+###
+### INCAR
+### POSCAR
+### POTCAR
+### KPOINTS
+### vasp.script
+### fort.188
